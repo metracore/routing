da72f69cb546ff078fdcfb162a12bfb9a1b6b2e4
SandeepKr <code@sandeepkr.com>
2023-07-07 18:49:17 +0530

    init

---
+++ Router.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Metracore\Routing;
+
+/*
+ * Router class for Metracore\Routing commponent nitrovel\framework 
+ */
+
+class Router{
+    
+    public function __construct(){
+
+    }
+
+}
+
+
+?>
